7cf680e6f5fe22b327d1a1d97e9e9499cfe2dc20
Vladimir Durana <vladimir.durana@unitedclassifieds.sk>
2022-01-23 11:17:41 +0100

    feat: added basic EsProperty.ts decorator functions

--- src/decorators/EsProperty.ts
+++ src/decorators/EsProperty.ts
@@ -1,9 +1,46 @@
-export interface IEsPropertyOptions{
+import { EsType } from './EsTypes';
+import 'reflect-metadata';
 
+export interface EsPropertyOptions {
+  analyzer?: Record<string, unknown>;
+  name?: string;
 }
 
-export function EsProperty(name: string, options: IEsPropertyOptions = {}): PropertyDecorator{
-  return (target, name) => {
+export interface EsPropertyTypedOptions extends EsPropertyOptions {
+  type: EsType;
+}
 
-  }
+export function EsProperty(options: EsPropertyTypedOptions): PropertyDecorator;
+
+export function EsProperty(
+  type: EsType,
+  options?: EsPropertyTypedOptions,
+): PropertyDecorator;
+
+export function EsProperty(
+  option1?: EsType | EsPropertyTypedOptions,
+  option2?: EsPropertyOptions,
+): PropertyDecorator {
+  return (target, name) => {
+    let propertyOptions: EsPropertyTypedOptions;
+
+    if (option1 instanceof Object) {
+      propertyOptions = option1;
+    } else if (option2 instanceof Object) {
+      propertyOptions = Object.assign({ type: option1 }, option2);
+    } else {
+      throw new Error('Not valid elastic property options');
+    }
+
+    Object.assign(
+      {
+        name: name,
+      },
+      propertyOptions,
+    );
+
+    const properties = Reflect.getMetadata(EsProperty.name, target) || [];
+    properties.push(propertyOptions);
+    return Reflect.defineMetadata(EsProperty.name, properties, target);
+  };
 }

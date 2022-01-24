a775b2dd16b03f2b77f2f46f5d365d2f1d4ece0b
Vladimir Durana <vladimir.durana@unitedclassifieds.sk>
2022-01-24 21:58:05 +0100

    feat: added repository interface

---
+++ src/repository/EsRepository.interface.ts
@@ -0,0 +1,41 @@
+export interface EsRequestBulkOptions {
+  throwException?: true;
+}
+
+export interface EsRepositoryInterface<Entity = unknown> {
+  create(entity: Entity): Promise<Entity>;
+
+  createMultiple(
+    entities: Entity[],
+    requestBulkOptions: EsRequestBulkOptions,
+  ): Promise<Entity[]>;
+
+  index(entity: Entity): Promise<Entity>;
+
+  indexMultiple(
+    entities: Entity[],
+    requestBulkOptions: EsRequestBulkOptions,
+  ): Promise<Entity[]>;
+
+  save(entity: Entity): Promise<Entity>;
+
+  saveMultiple(
+    entities: Entity[],
+    requestBulkOptions: EsRequestBulkOptions,
+  ): Promise<Entity[]>;
+
+  delete(id: string[]): Promise<boolean>;
+
+  deleteMultiple(
+    entity: Entity,
+    requestBulkOptions: EsRequestBulkOptions,
+  ): Promise<Entity>;
+
+  findOne(query): Promise<Entity>;
+
+  findOneOrFail(query): Promise<Entity>;
+
+  find(query): Promise<Entity[]>;
+
+  findById(id: string | number): Promise<Entity>;
+}

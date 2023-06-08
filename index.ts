62d11af8442f44531572e68b15c343e7b19fa564
Vladimir Durana <durana.vladimir99@gmail.com>
2023-06-08 15:22:27 +0200

    feat: add exports into default index file

---
+++ index.ts
@@ -0,0 +1,47 @@
+export * from './src/decorators/EsEntity';
+export * from './src/decorators/EsId';
+export * from './src/decorators/EsProperty';
+
+export * from './src/entity/defaultIdGenerator';
+export * from './src/entity/Normalized.entity';
+
+export * from './src/exceptions/EsBulkException';
+export * from './src/exceptions/EsEntityNotFoundException';
+export * from './src/exceptions/EsException';
+export * from './src/exceptions/EsValidationException';
+
+export * from './src/factory/Factory.provider';
+
+export * from './src/query/collapse';
+export * from './src/query/common';
+export * from './src/query/fields';
+export * from './src/query/filter';
+export * from './src/query/fulltextQueries';
+export * from './src/query/geoQueries';
+export * from './src/query/common';
+export * from './src/query/highlights';
+export * from './src/query/intervals';
+export * from './src/query/query';
+export * from './src/query/rescore';
+export * from './src/query/sort';
+export * from './src/query/source';
+export * from './src/query/suggestion';
+export * from './src/query/termQueries';
+
+export * from './src/repository/EsBulkResponseInterface';
+export * from './src/repository/EsRepository.interface';
+export * from './src/repository/EsRepository';
+
+export * from './src/types/Class.type';
+export * from './src/types/Es.type';
+export * from './src/types/EsClassTypeOptions.interface';
+export * from './src/types/EsFieldPropertyOptions.interface';
+export * from './src/types/EsIndex.interface';
+export * from './src/types/EsMapping.interface';
+export * from './src/types/EsMetaData.interface';
+export * from './src/types/EsPropertyOptions.intarface';
+
+export * from './src/utils/common';
+export * from './src/utils/EntityTransformer';
+export * from './src/utils/MetaLoader';
+export * from './src/utils/SchemaManager';

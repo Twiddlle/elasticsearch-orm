42d8ba111994de631c2ebd16be9e533b9573717e
David Livingston <David.Livingston+mbpnov23@BlueHalo.com>
2025-04-07 11:54:29 -0500

    refactor: set `.cause` on Error

--- src/exceptions/EsException.ts
+++ src/exceptions/EsException.ts
@@ -3,6 +3,16 @@ import {ErrorCause} from "@elastic/elasticsearch/lib/api/types";
 export class EsException extends Error implements ErrorCause{
   public constructor(message?: string, public originalError?: Error) {
     super(message);
+
+    // `originalError` is conventional in ES6 / ES2015 which we target
+    // `cause` is standard in Node 16.9+ / ES2021+
+    Object.defineProperty(this, "cause", {
+      get: () => this.originalError,
+      set: (newVal: unknown) => {
+        this.originalError = newVal as Error;
+      },
+      enumerable: false,
+    });
   }
 
   caused_by: ErrorCause;
@@ -11,4 +21,7 @@ export class EsException extends Error implements ErrorCause{
   stack_trace: string;
   suppressed: ErrorCause[];
   type: string;
+
+  // this property exists on `Error` in ES2021+
+  cause: unknown;
 }

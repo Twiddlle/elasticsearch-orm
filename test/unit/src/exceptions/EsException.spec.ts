8b7f03be02d4e4add255dc0d90135540d5cb75b4
David Livingston <git@dliv.org>
2025-04-07 15:51:06 -0500

    refactor: set `.cause` on Error (#2)
    * refactor: set `.cause` on Error
    
    * typo
    
    ---------
    
    Co-authored-by: David Livingston <David.Livingston+mbpnov23@BlueHalo.com>

---
+++ test/unit/src/exceptions/EsException.spec.ts
@@ -0,0 +1,25 @@
+import { EsException } from '../../../../src/exceptions/EsException';
+
+describe('EsException', () => {
+  it('sets both originalCause and cause', () => {
+    const original = new Error('original');
+    const wrapper = new EsException('wrapper', original);
+
+    // conventional ES6
+    expect(wrapper.originalError).toBe(original);
+    // standard ES2021
+    expect(wrapper.cause).toBe(original);
+  });
+
+  // this happens in `makeEsException`
+  it('setting originalError also sets cause', () => {
+    const wrapper = new EsException('wrapper');
+    const original = new Error('original');
+    wrapper.originalError = original;
+
+    // conventional ES6
+    expect(wrapper.originalError).toBe(original);
+    // standard ES2021
+    expect(wrapper.cause).toBe(original);
+  });
+});
